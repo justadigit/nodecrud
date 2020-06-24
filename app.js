9a2bd6d0a7758001e2f2bd1ceb66f0b810d8cd6e
justadigit <letmecode123456@gmail.com>
2020-06-25 01:21:52 +0630

    connect to mongodb

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
+//connect to DB
+const DBURI = "mongodb+srv://naywin:naywin23@cluster0-urly7.mongodb.net/nodecrash?retryWrites=true&w=majority";
+
+mongoose.connect(DBURI,{ useNewUrlParser: true,useUnifiedTopology: true })
+.then(data=>console.log('success to connect'))
+.catch(err=>console.log(err));
+
+
 // express app
 const app = express();
+
 
 //register view engine
 app.set("view engine","ejs");
@@ -18,17 +30,25 @@ app.use(express.static('public'))
 
 //home page route
 app.get('/',(req,res)=>{
-    const blogs = [
-        {title:"New Schedule",snippet:"Lorem ipsum dolor sit amet consectetur adipisicing elit."},
-        {title:"How to do",snippet:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
-        {title:"Work Early",snippet:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
-    ]
-    res.render('index',{title:'Home',blogs})
+    res.redirect('/blogs');
 })
 
 //about page route
 app.get('/about',(req,res)=>{
    res.render('about',{title:"About"})
+})
+
+//blogs
+app.get('/blogs',(req,res)=>{
+    Blog.find().sort({createdAt:-1})
+    .then(data=>{
+        res.render('index',{
+            title:"All Blogs",
+            blogs:data
+        })
+    }).catch(err=>{
+        console.log(err)
+    })
 })
 
 //create blog route
@@ -37,10 +57,30 @@ app.get('/blogs/create',(req,res)=>{
 })
 
 
+//add new blog
+app.get('/add-blog',(req,res)=>{
+    const blog =new Blog({
+        title:"my new blog",
+        snippet:"note more for my new blog",
+        body:"more and more for my new blog"
+    });
+    blog.save()
+    .then(data=>res.send(data))
+    .catch(err=>res.send(err));
+})
+
+//reterive all blogs
+app.get('/all-blogs',(req,res)=>{
+    Blog.find()
+    .then(data=>res.send(data))
+    .catch(err=>res.send(err))
+})
+
 //404 page
 app.use((req,res)=>{
     res.status(404).render('404',{title:'404'});
 })
+
 
 //listen app for request
 app.listen(PORT,()=>{
